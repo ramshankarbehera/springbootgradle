8216f5141f005ab0011cbaaae95f240b2b0aa81f
ramshankar <ramshankar.behera@swiggy.in>
2020-06-28 17:15:25 +0530

    added new 1

--- src/test/java/unitTest/TestMyCode.java
+++ src/test/java/unitTest/TestMyCode.java
@@ -1,8 +1,11 @@
 package com.javadeveloperzone;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Tag;
 
 class TestMyCode {
 
+
+    @Tag("fast")
     @Test
     void justAnExample() {
         System.out.println("This test method should be run");
