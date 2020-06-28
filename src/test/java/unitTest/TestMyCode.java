c866299df9a986cfe97b0cecf8c0b6be27981860
ramshankar <ramshankar.behera@swiggy.in>
2020-06-28 15:00:26 +0530

    added

--- src/test/java/unitTest/TestMyCode.java
+++ src/test/java/unitTest/TestMyCode.java
@@ -4,9 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 public class TestMyCode {
 
-    String message = "Hello World";
-    MessageUtil messageUtil = new MessageUtil(message);
-
     @Test
     public void testPrintMessage() {
         System.out.println("This test method should be run");
